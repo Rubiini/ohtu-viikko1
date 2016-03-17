838eea57f65e12b5cd05ee94495c1b878a69c794
Samu Kauppinen <samu.kauppinen@helsinki.fi>
2016-03-17 16:59:46 +0200

    test should work again

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -94,7 +94,7 @@ public class VarastoTest {
     
     @Test
     public void otetaanNegatiivinenLuku() {
-        //varasto.lisaaVarastoon(4);
+        varasto.lisaaVarastoon(4);
         varasto.otaVarastosta(-2);
         
         
